a39aba62d05f1b7d1148f5a986018c0ba431d540
jannatul Fardus Retu <jannatulfardusretu@jannatuls-mbp.lan>
2019-09-07 15:36:57 -0400

    first commit

---
+++ testngProject/src/test/java/com/bit/ui/test/ReggretionTest.java
@@ -0,0 +1,17 @@
+package com.bit.ui.test;
+
+import org.testng.annotations.Test;
+
+public class ReggretionTest extends BaseTest{
+	@Test
+	public void test2() {
+		System.out.println("test2");
+		
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+		
+	}
+
+}
---
+++ testngProject/src/test/java/com/bit/ui/test/SmokeTest.java
@@ -0,0 +1,16 @@
+package com.bit.ui.test;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest {
+	
+	@Test
+	public void test1() {
+		System.out.println("test1");
+		
+	}
+//shortcut way to copy past classname and pakgname togethr
+	//right click on a class name than  select copy cualify name thats it 
+	
+	
+}
